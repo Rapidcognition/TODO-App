131d53d8bced419af247397ee05dc4bcf1757760
Mathias <37350380+Rapidcognition@users.noreply.github.com>
2019-05-09 11:43:45 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+/* create todo-item from the object,
+    <li class="completed">
+        <div class="view">
+            <input class="toggle" type="checkbox" checked>
+            <label>Stuff</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="stuff">
+    </li>
+*/
